18a65e29280631dacd984f9d093ebbcbf56ce9f2
housseine <housseine2014tassa@gmail.com>
2018-09-05 10:30:02 +0100

    travis

--- build.sh
+++ build.sh
@@ -6,3 +6,16 @@ if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [[ $TRAVIS_BRANCH = 'master'  ||  ${T
       -Dsonar.projectKey=${SONAR_PROJECT} \
       -Dsonar.organization=${SONAR_ORGANIZATION}
 fi
+elif [ ${TRAVIS_PULL_REQUEST} != 'false' ]; then
+      echo 'Build and analyze pull request'
+      mvn -B clean verify sonar:sonar \
+      -Dsonar.host.url=${SONAR_URL} \
+      -Dsonar.projectKey=${SONAR_PROJECT} \
+      -Dsonar.organization=${SONAR_ORGANIZATION} \
+      -Dsonar.login=${SONAR_LOGIN} \
+      -Dsonar.github.oauth=${SONAR_GITHUB_OAUTH} \
+      -Dsonar.pullrequest.github.repository=${TRAVIS_REPO_SLUG}  \
+      -Dsonar.pullrequest.provider=GitHub  \
+      -Dsonar.pullrequest.branch=${TRAVIS_BRANCH}  \
+      -Dsonar.pullrequest.key=${TRAVIS_PULL_REQUEST};
+fi
