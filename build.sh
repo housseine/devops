d8d4ebd830fdb2e32a3c5c8b18bc4e9c1cfaf7b9
housseine <housseine2014tassa@gmail.com>
2018-09-05 11:32:27 +0100

    [modification de fichier build.sh]

--- build.sh
+++ build.sh
@@ -16,6 +16,6 @@ elif [ ${TRAVIS_PULL_REQUEST} != 'false' ]; then
       -Dsonar.github.oauth=${7308820e27aaae162b89234be65dbaf7feeba422} \
       -Dsonar.pullrequest.github.repository=${housseine/devops}  \
       -Dsonar.pullrequest.provider=GitHub  \
-      -Dsonar.pullrequest.branch=${develop}  \
+      -Dsonar.pullrequest.branch=${TRAVIS_BRANCH}  \
       -Dsonar.pullrequest.key=${TRAVIS_PULL_REQUEST};
 fi
