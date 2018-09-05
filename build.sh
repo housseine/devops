cc64d0a0a45ec8dfd66e982973c38d4caa5898ed
housseine <housseine2014tassa@gmail.com>
2018-09-05 20:10:16 +0100

    message

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [[ $TRAVIS_BRANCH = 'master'  ||  ${TRAVIS_BRANCH} = 'develop' ]]; then
       echo 'Checking Quality Gates'
-      mvn -B clean verify sonar:sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_LOGIN} -Dsonar.projectKey=${SONAR_PROJECT} -Dsonar.organization=${SONAR_ORGANIZATION}
+      mvn -B clean verify sonar:sonar -Dsonar.host.url=${SONAR_URL}  -Dsonar.projectKey=${SONAR_PROJECT} -Dsonar.organization=${SONAR_ORGANIZATION}
 elif [ ${TRAVIS_PULL_REQUEST} != 'false' ]; then
       echo 'Build and analyze pull request'
-      mvn -B clean verify sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.projectKey=housseine_devops -Dsonar.organization=housseine-github -Dsonar.login=${SONAR_LOGIN} -Dsonar.github.oauth=7308820e27aaae162b89234be65dbaf7feeba422  -Dsonar.pullrequest.github.repository=housseine/devops -Dsonar.pullrequest.provider=GitHub -Dsonar.pullrequest.branch=${TRAVIS_BRANCH} -Dsonar.pullrequest.key=${TRAVIS_PULL_REQUEST};
+      mvn -B clean verify sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.projectKey=housseine_devops -Dsonar.organization=housseine-github  -Dsonar.github.oauth=7308820e27aaae162b89234be65dbaf7feeba422  -Dsonar.pullrequest.github.repository=housseine/devops -Dsonar.pullrequest.provider=GitHub -Dsonar.pullrequest.branch=${TRAVIS_BRANCH} -Dsonar.pullrequest.key=${TRAVIS_PULL_REQUEST};
 fi
