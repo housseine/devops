1558a8abf783171e05e33637281541278176c621
housseine <housseine2014tassa@gmail.com>
2018-09-05 09:27:44 +0100

    modif

---
+++ src/test/java/ma/craft/devops/example/devops/DevopsApplicationTests.java
@@ -0,0 +1,24 @@
+package ma.craft.devops.example.devops;
+
+import ma.craft.devops.example.devops.controller.OpsController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DevopsApplicationTests {
+
+	@Autowired
+	OpsController controller;
+
+	@Test
+	public void contextLoads() {
+		assertThat(controller).isNotNull();
+	}
+
+}
